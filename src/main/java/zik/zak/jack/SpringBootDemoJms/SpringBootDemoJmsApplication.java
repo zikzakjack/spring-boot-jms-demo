727209d1be735296212d05cfbcd270bef095ee9b
Kalyan <kalyansmiles@gmail.com>
2016-10-18 09:15:59 -0400

    Initial commit of spring boot jms demo

---
+++ src/main/java/zik/zak/jack/SpringBootDemoJms/SpringBootDemoJmsApplication.java
@@ -0,0 +1,12 @@
+package zik.zak.jack.SpringBootDemoJms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoJmsApplication.class, args);
+	}
+}
