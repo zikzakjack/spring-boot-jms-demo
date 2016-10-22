eb0277ae68c34956a8393bf08f7b45d9b260b3e5
Kalyan <kalyansmiles@gmail.com>
2016-10-22 14:52:54 -0400

    ScheduledTask that generates random transactions for testdata

--- src/main/java/zik/zak/jack/SpringBootDemoJms/tasks/ScheduledTasks.java
+++ src/main/java/zik/zak/jack/SpringBootDemoJms/tasks/ScheduledTasks.java
@@ -1,40 +1,30 @@
 package zik.zak.jack.SpringBootDemoJms.tasks;
 
-import java.util.Date;
-import java.util.Random;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import zik.zak.jack.SpringBootDemoJms.domain.TransactionAlert;
-import zik.zak.jack.SpringBootDemoJms.jms.QueueSender;
+import zik.zak.jack.SpringBootDemoJms.services.ATMService;
 
+/**
+ * ScheduledTask that generates random transactions for testdata
+ * 
+ * @author kalyan
+ *
+ */
 @Component
 public class ScheduledTasks {
 
-  private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+  ATMService atmService;
 
-  QueueSender queueSender;
-  
   @Autowired
-  public void setQueueSender(QueueSender queueSender) {
-    this.queueSender = queueSender;
+  public ScheduledTasks(ATMService atmService) {
+    this.atmService = atmService;
   }
-
 
   @Scheduled(initialDelay = 5000, fixedRate = 10000)
-  public void reportCurrentTime() {
-    
-    // Create Random Transaction Alerts as test data
-    TransactionAlert alert = new TransactionAlert(TransactionAlert.TransactionType.get(new Random().nextInt(2)),
-        (int) ((Math.random() * 900000) + 100) / 100.0, new Date());
-    logger.info("<=============================================> ");
-    logger.info("Transaction Detected => " + alert);
-    
-    // Post the Transaction to a Destination
-    queueSender.send(alert);
+  public void generateDummyTransactions() {
+    atmService.transact();
   }
+
 }
