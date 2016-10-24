6a46bd99e263331e9804791ed83e594d536a2ec6
Kalyan <kalyansmiles@gmail.com>
2016-10-24 07:35:55 -0400

    Tweak EmailQueueListenerImpl Listener to send email once the transaction alerts are read

--- src/main/java/zik/zak/jack/SpringBootDemoJms/jms/impl/EmailQueueListenerImpl.java
+++ src/main/java/zik/zak/jack/SpringBootDemoJms/jms/impl/EmailQueueListenerImpl.java
@@ -4,11 +4,13 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import zik.zak.jack.SpringBootDemoJms.domain.TransactionAlert;
 import zik.zak.jack.SpringBootDemoJms.jms.QueueListener;
+import zik.zak.jack.SpringBootDemoJms.mail.EmailService;
 
 /**
  * Listener class that reads messages from EmailQueue
@@ -21,6 +23,9 @@ public class EmailQueueListenerImpl implements QueueListener {
 
   private static final Logger logger = LoggerFactory.getLogger(EmailQueueListenerImpl.class);
 
+  @Autowired
+  EmailService emailService;
+  
   @PostConstruct
   public void init() {
     logger.info("EmailQueueListenerImpl Listener is Initialized...");
@@ -29,6 +34,10 @@ public class EmailQueueListenerImpl implements QueueListener {
   @JmsListener(destination = "${trans.alert.email.queue}", containerFactory = "queueJmsListenerContainerFactory")
   public void receiveMessage(TransactionAlert alert) {
     logger.info("Sending Email => <" + alert + ">");
+    // In a typical case, from address would be configured in application.properties
+    // to address would be retrieved from user or account details.
+    // I am hardcoding in this case for simplicity 
+    emailService.prepareAndSend("from_changeme@gmail.com", "to_changeme@gmail.com", alert.toString());
   }
 
 }
